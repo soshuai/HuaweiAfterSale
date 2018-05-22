a2bef0209cce2811bc931af19d5ca64c92059b7e
soshuai <601647787@qq.com>
2018-05-22 12:03:36 +0800

    本地gps

--- app/src/main/java/cherish/cn/huaweiaftersale/MainActivity.java
+++ app/src/main/java/cherish/cn/huaweiaftersale/MainActivity.java
@@ -1,7 +1,16 @@
 package cherish.cn.huaweiaftersale;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -17,18 +26,73 @@ import cherish.cn.huaweiaftersale.fragment.MineFragment;
 import cherish.cn.huaweiaftersale.fragment.OrderHandleFragment;
 import cherish.cn.huaweiaftersale.fragment.OrderSearchFragment;
 import cherish.cn.huaweiaftersale.jpush.JPushHelper;
+import cherish.cn.huaweiaftersale.util.AndroidGpsLocation;
 import cherish.cn.huaweiaftersale.util.DoubleClickExitHelper;
+import cherish.cn.huaweiaftersale.util.LogUtils;
 
 public class MainActivity extends BaseMultiFragsActivity {
     private MainPagerTabContext mTabContext[];
     private DoubleClickExitHelper mExitHelper;
-    private int TAB_COUNT=3;
+    private int TAB_COUNT = 3;
+    private LocationManager mLocationManager;//位置管理器
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         JPushHelper.resumePush();
+//        startSendLocation();
+//        AndroidGpsLocation.getInstance(mContext);
+//        AndroidGpsLocation.isOpenGps();
+//        AndroidGpsLocation.formListenerGetLocation();
+//        AndroidGpsLocation.getLocation();
     }
+
+    /*private void startSendLocation() {
+//获取到位置管理器实例
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        //获取到GPS_PROVIDER
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            androidToast("未获取到GPS权限");
+            return;
+        }
+        Location location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        //侦听位置发生变化，2000毫秒更新一次，位置超过8米也更新一次
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 8, new LocationListener() {
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                // 当GPS Location Provider可用时，更新位置
+                if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    androidToast("未获取到GPS权限");
+                    return;
+                }
+                updata(mLocationManager.getLastKnownLocation(provider));
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                // TODO Auto-generated method stub
+
+            }
+            @Override
+            public void onLocationChanged(Location location) {
+                // 当GPS定位信息发生改变时，更新位置
+                updata(location);
+            }
+        });
+        //更新位置信息显示到TextView
+        updata(location);
+    }
+
+    private void updata(Location location) {
+        Log.i("Location","纬度="+location.getLatitude()+" 经度="+location.getLongitude());
+    }*/
 
     @Override
     protected int getLayoutId() {
